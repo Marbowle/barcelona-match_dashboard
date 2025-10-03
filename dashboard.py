66acdd5f2d46892d2e1d3137d88d7b147f23f8af
Marta Domagała <mahomegol@gmail.com>
2025-10-03 10:31:21 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import queries
+
+st.title("Match Dashboard")
+
+players_df = queries.get_players()
